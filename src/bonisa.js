b57aea692d0fc8b69c1efa4d6c06c7a1a361429b
zmdy <zmdy@zmdy>
2018-05-23 16:58:09 -0300

    Removing modules - postModule and PDF

--- src/bonisa.js
+++ src/bonisa.js
@@ -397,9 +397,12 @@
                 
 		// Make sure we've got all the DOM elements we need
 		setupDOM();
-
-		// Listen to messages posted to this window
-		setupPostMessage();
+                
+                /****************** | DISABLED | ******************
+                *
+                * Function setupPostMessage()
+                * 
+                **************************************************/
 
 		// Prevent the slides from being scrolled out of view
 		setupScrollPrevention();
@@ -488,9 +491,9 @@
                 /*** PAUSE MODE ***/
                     // Overlay graphic which is displayed during the paused mode
                     createSingletonNode( dom.wrapper, 'div', 'pause-overlay', null );
-
+                
 		dom.wrapper.setAttribute( 'role', 'application' );
-
+                
 		// There can be multiple instances of controls throughout the page
 		dom.controlsLeft = toArray( document.querySelectorAll( '.navigate-left' ) );
 		dom.controlsRight = toArray( document.querySelectorAll( '.navigate-right' ) );
@@ -563,141 +566,17 @@
 		return text;
 
 	}
-
-	/**
-	 * Configures the presentation for printing to a static
-	 * PDF.
-	 */
-	function setupPDF() {
-
-		var slideSize = getComputedSlideSize( window.innerWidth, window.innerHeight );
-
-		// Dimensions of the PDF pages
-		var pageWidth = Math.floor( slideSize.width * ( 1 + config.margin ) ),
-			pageHeight = Math.floor( slideSize.height * ( 1 + config.margin ) );
-
-		// Dimensions of slides within the pages
-		var slideWidth = slideSize.width,
-			slideHeight = slideSize.height;
-
-		// Let the browser know what page size we want to print
-		injectStyleSheet( '@page{size:'+ pageWidth +'px '+ pageHeight +'px; margin: 0px;}' );
-
-		// Limit the size of certain elements to the dimensions of the slide
-		injectStyleSheet( '.bonisa section>img, .bonisa section>video, .bonisa section>iframe{max-width: '+ slideWidth +'px; max-height:'+ slideHeight +'px}' );
-
-		document.body.classList.add( 'print-pdf' );
-		document.body.style.width = pageWidth + 'px';
-		document.body.style.height = pageHeight + 'px';
-
-		// Make sure stretch elements fit on slide
-		layoutSlideContents( slideWidth, slideHeight );
-
-		// Add each slide's index as attributes on itself, we need these
-		// indices to generate slide numbers below
-		toArray( dom.wrapper.querySelectorAll( HORIZONTAL_SLIDES_SELECTOR ) ).forEach( function( hslide, h ) {
-			hslide.setAttribute( 'data-index-h', h );
-
-			if( hslide.classList.contains( 'stack' ) ) {
-				toArray( hslide.querySelectorAll( 'section' ) ).forEach( function( vslide, v ) {
-					vslide.setAttribute( 'data-index-h', h );
-					vslide.setAttribute( 'data-index-v', v );
-				} );
-			}
-		} );
-
-		// Slide and slide background layout
-		toArray( dom.wrapper.querySelectorAll( SLIDES_SELECTOR ) ).forEach( function( slide ) {
-
-			// Vertical stacks are not centred since their section
-			// children will be
-			if( slide.classList.contains( 'stack' ) === false ) {
-				// Center the slide inside of the page, giving the slide some margin
-				var left = ( pageWidth - slideWidth ) / 2,
-					top = ( pageHeight - slideHeight ) / 2;
-
-				var contentHeight = slide.scrollHeight;
-				var numberOfPages = Math.max( Math.ceil( contentHeight / pageHeight ), 1 );
-
-				// Adhere to configured pages per slide limit
-				numberOfPages = Math.min( numberOfPages, config.pdfMaxPagesPerSlide );
-
-				// Center slides vertically
-				if( numberOfPages === 1 && config.center || slide.classList.contains( 'center' ) ) {
-					top = Math.max( ( pageHeight - contentHeight ) / 2, 0 );
-				}
-
-				// Wrap the slide in a page element and hide its overflow
-				// so that no page ever flows onto another
-				var page = document.createElement( 'div' );
-				page.className = 'pdf-page';
-				page.style.height = ( ( pageHeight + config.pdfPageHeightOffset ) * numberOfPages ) + 'px';
-				slide.parentNode.insertBefore( page, slide );
-				page.appendChild( slide );
-
-				// Position the slide inside of the page
-				slide.style.left = left + 'px';
-				slide.style.top = top + 'px';
-				slide.style.width = slideWidth + 'px';
-
-				if( slide.slideBackgroundElement ) {
-					page.insertBefore( slide.slideBackgroundElement, slide );
-				}
-
-				// Inject notes if `showNotes` is enabled
-				if( config.showNotes ) {
-
-					// Are there notes for this slide?
-					var notes = getSlideNotes( slide );
-					if( notes ) {
-
-						var notesSpacing = 8;
-						var notesLayout = typeof config.showNotes === 'string' ? config.showNotes : 'inline';
-						var notesElement = document.createElement( 'div' );
-						notesElement.classList.add( 'speaker-notes' );
-						notesElement.classList.add( 'speaker-notes-pdf' );
-						notesElement.setAttribute( 'data-layout', notesLayout );
-						notesElement.innerHTML = notes;
-
-						if( notesLayout === 'separate-page' ) {
-							page.parentNode.insertBefore( notesElement, page.nextSibling );
-						}
-						else {
-							notesElement.style.left = notesSpacing + 'px';
-							notesElement.style.bottom = notesSpacing + 'px';
-							notesElement.style.width = ( pageWidth - notesSpacing*2 ) + 'px';
-							page.appendChild( notesElement );
-						}
-
-					}
-
-				}
-
-				// Inject slide numbers if `slideNumbers` are enabled
-				if( config.slideNumber && /all|print/i.test( config.showSlideNumber ) ) {
-					var slideNumberH = parseInt( slide.getAttribute( 'data-index-h' ), 10 ) + 1,
-						slideNumberV = parseInt( slide.getAttribute( 'data-index-v' ), 10 ) + 1;
-
-					var numberElement = document.createElement( 'div' );
-					numberElement.classList.add( 'slide-number' );
-					numberElement.classList.add( 'slide-number-pdf' );
-					numberElement.innerHTML = formatSlideNumber( slideNumberH, '.', slideNumberV );
-					page.appendChild( numberElement );
-				}
-			}
-
-		} );
-
-		// Show all fragments
-		toArray( dom.wrapper.querySelectorAll( SLIDES_SELECTOR + ' .fragment' ) ).forEach( function( fragment ) {
-			fragment.classList.add( 'visible' );
-		} );
-
-		// Notify subscribers that the PDF layout is good to go
-		dispatchEvent( 'pdf-ready' );
-
-	}
-
+        
+        
+        /****************** | DISABLED | ******************
+        *
+        * Function setupPDF()
+        * 
+        * Configures the presentation for printing to a static
+        * PDF.
+        * 
+        **************************************************/
+        
 	/**
 	 * This is an unfortunate necessity. Some actions – such as
 	 * an input field being focused in an iframe or using the
@@ -761,9 +640,6 @@
 	 * slide no matter if the given slide has visible background.
 	 */
 	function createBackgrounds() {
-
-		var printMode = isPrintingPDF();
-
 		// Clear prior backgrounds
 		dom.background.innerHTML = '';
 		dom.background.classList.add( 'no-transition' );
@@ -899,9 +775,12 @@
 		return element;
 
 	}
-
-	/**
-	 * Registers a listener to postMessage events, this makes it
+        
+        /****************** | DISABLED | ******************
+         *
+         * Function setupPostMessage()
+         * 
+         * Registers a listener to postMessage events, this makes it
 	 * possible to call all bonisa.js API methods from another
 	 * window. For example:
 	 *
@@ -909,26 +788,8 @@
 	 *   method: 'slide',
 	 *   args: [ 2 ]
 	 * }), '*' );
-	 */
-	function setupPostMessage() {
-
-		if( config.postMessage ) {
-			window.addEventListener( 'message', function ( event ) {
-				var data = event.data;
-
-				// Make sure we're dealing with JSON
-				if( typeof data === 'string' && data.charAt( 0 ) === '{' && data.charAt( data.length - 1 ) === '}' ) {
-					data = JSON.parse( data );
-
-					// Check if the requested method can be found
-					if( data.method && typeof Bonisa[data.method] === 'function' ) {
-						Bonisa[data.method].apply( Bonisa, data.args );
-					}
-				}
-			}, false );
-		}
-
-	}
+         **************************************************/
+        
 
 	/**
 	 * Applies the configuration settings from the config
@@ -1048,7 +909,7 @@
 
 		// Slide numbers
 		var slideNumberDisplay = 'none';
-		if( config.slideNumber && !isPrintingPDF() ) {
+		if( config.slideNumber ) {
 			if( config.showSlideNumber === 'all' ) {
 				slideNumberDisplay = 'block';
 			}
@@ -1452,15 +1313,15 @@
 		return height;
 
 	}
-
-	/**
-	 * Checks if this instance is being used to print a PDF.
-	 */
-	function isPrintingPDF() {
-
-		return ( /print-pdf/gi ).test( window.location.search );
-
-	}
+        
+        
+        /****************** | DISABLED | ******************
+        *
+        * Function isPrintingPDF()
+        * 
+        * Checks if this instance is being used to print a PD
+        * 
+        **************************************************/
 
 	/**
 	 * Hides the address bar if we're on a mobile device.
@@ -1714,7 +1575,7 @@
 	 */
 	function layout() {
 
-		if( dom.wrapper && !isPrintingPDF() ) {
+		if( dom.wrapper ) {
 
 			var size = getComputedSlideSize();
 
@@ -2531,8 +2392,6 @@
 		var slides = toArray( dom.wrapper.querySelectorAll( selector ) ),
 			slidesLength = slides.length;
 
-		var printMode = isPrintingPDF();
-
 		if( slidesLength ) {
 
 			// Should the index loop?
@@ -2563,12 +2422,6 @@
 				// If this element contains vertical slides
 				if( element.querySelector( 'section' ) ) {
 					element.classList.add( 'stack' );
-				}
-
-				// If we're printing static slides, all slides are "present"
-				if( printMode ) {
-					element.classList.add( 'present' );
-					continue;
 				}
 
 				if( i < index ) {
@@ -2650,11 +2503,6 @@
 				viewDistance = isOverview() ? 6 : 2;
 			}
 
-			// All slides need to be visible when exporting to PDF
-			if( isPrintingPDF() ) {
-				viewDistance = Number.MAX_VALUE;
-			}
-
 			for( var x = 0; x < horizontalSlidesLength; x++ ) {
 				var horizontalSlide = horizontalSlides[x];
 
@@ -2726,7 +2574,7 @@
 	 */
 	function updateNotes() {
 
-		if( config.showNotes && dom.speakerNotes && currentSlide && !isPrintingPDF() ) {
+		if( config.showNotes && dom.speakerNotes && currentSlide ) {
 
 			dom.speakerNotes.innerHTML = getSlideNotes() || '<span class="notes-placeholder">No notes on this slide.</span>';
 
