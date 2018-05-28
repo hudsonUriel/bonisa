63d20475014f409cf69317beec5e187ecf1166c4
zmdy <zmdy@zmdy>
2018-05-27 21:21:42 -0300

    Creating basic ASI function (anonymous self-invoking function) and test function help()

--- src/BONISA.js
+++ src/BONISA.js
@@ -1,7 +1,55 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*!
+ * bonisa.js
+ * AGPL-3.0 licensed
+ *
+ * 
+ * --> this file is a bonisa.js modification
  */
 
-
+/*
+ * *************** | FUNCTION | ***************
+ * 
+ * Anonymous self-invoking function
+ * 
+ * ********************************************
+ * 
+ * This function recieves the Window object
+ * and an anonymous function that defines
+ * all Bonisa features, making it available
+ * to the global scope
+ * 
+ * ********************************************
+ * 
+ * @param {Window} root
+ * @param {function} factory
+ * @returns {void}
+ * 
+ */
+(function(root, factory){
+    window.Bonisa = factory();
+}(this, function(){
+    /* ---------- INITIAL VARIABLES ---------- */
+    var 
+        // null object
+        Bonisa, 
+        
+        // Bonisa version and codename
+        VERSION = '1.0.0';
+    
+    /* --------------- FUNCTIONS --------------- */
+    help = function(){
+        return(
+            'Welcome to Bonisa ' + VERSION
+        );
+    };
+    
+    /* ------------------ API ------------------ */
+    Bonisa = {
+        version: VERSION,
+        
+        help: help
+    };
+    
+    /* ----------------- RETURNS ---------------- */
+    return Bonisa;
+}));
