1a2d5d52e045539675ee48270e8cde3396d5eac6
zmdyLinux <hudsonuriel@gmail.com>
2019-08-21 12:52:40 -0300

    Fixing relative references...

--- src/relativeReference.js
+++ src/relativeReference.js
@@ -1,20 +1,54 @@
 function relativize(content){
+	/*
+	* Links are structure that:
+	*
+	* ^(\w{1,}\u003A\u002F{2}|(\w{1,}|\u002E{1,2})(\u002F|\u002E)){1,}(\w{1,}|\w{1,}\u002F|\w{1,}\W{1,}\w{1,}\u002E\w{1,})$
+	*
+	* ---
+	* 1. Begins with either:
+	*	a. protocol://
+	* b. ./
+	* c. ../
+	* d. folder/
+	* e. site.
+	*
+	* REGEX: ^(\w{1,}\u003A\u002F{2}|(\w{1,}|\u002E{1,2})(\u002F|\u002E))
+	*
+	* ---
+	* 2. Any of the itens of 1 can be repeated multiple times
+	* a. protocol://site.site.com
+	* b. ./folder/folder2/file.png
+	* c. folder/folder2/file.png
+	* d. site.com.br
+	*
+	* REGEX: ^(\w{1,}\u003A\u002F{2}|(\w{1,}|\u002E{1,2})(\u002F|\u002E)){1,}
+	*
+	* ---
+	*3. Ends with:
+	* a. word
+	* b file.format
+	* c. file-name.format
+	* d. word/
+	* e. file.format/
+	*
+	* REGEX: (\w{1,}|\w{1,}\u002F|\w{1,}\W{1,}\w{1,}\u002E\w{1,})$
+	*/
 	var 
 		content ,
 			
 		link,
 		curLocation = window.location,
 		rtr,
-			
-		regex = /(\u002e+|\D+|\d+)\u002F/g,
-		externalLink = /(\u002ecom|\u002egov|\u002enet)/g,
+	
+		regex = /^(\w{1,}\u003A\u002F{2}|(\w{1,}|\u002E{1,2})(\u002F|\u002E)){1,}(\w{1,}|\w{1,}\u002F|\w{1,}\W{1,}\w{1,}\u002E\w{1,})$/gm,
+		validLink = /\u002E(com|gov|org|net|info|io)/g,
 		hName = curLocation.hostname,
 		pName = curLocation.pathname.replace('/','').split('/')[0];
 	
 	rtr = content;
 	
-	// Split each [Link Reference](link/here) element found in the content
-	content = content.split(/(\u0028|\u0029|\u005b|\u005d| +)/g);
+	// Splits the content by " " or "[" or "]" or "(" or ")" or "{" or "}"
+	content = content.split(/( |\u0028|\u0029|\u005B|\u005D|\u007B|\u007D)/);
 	
 	// Search for links
 	for(let str in content){
@@ -23,9 +57,11 @@ function relativize(content){
 			link = content[str];
 			
 			// Tests if it is not an external link
-			if(!externalLink.test(link)){
+			if(!validLink.test(link)){
+				
 				var
-					newLink = curLocation.protocol + '//' + hName + '/' + pName + '/' +link.replace(/\u002e+\u002f/g, "");
+					newLink = Bonisa.location + '/' + link.replace(/\u002E+\u002F/g, "");
+				console.log(newLink);
 				
 				rtr = rtr.replace(link, newLink);
 			}
