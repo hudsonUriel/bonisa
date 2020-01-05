fe1659d2b999040afc18d09bbb3d3198609ff678
zmdy <hudsonuriel@gmail.com>
2020-01-05 19:09:35 -0300

    Joining generic functions (open file AND relativize content) to the main BONISA file

--- src/bonisa.js
+++ src/bonisa.js
@@ -521,6 +521,91 @@ var Bonisa = ( function(){
       link.href = Bonisa.styles[style];
     }
   };
+
+  /*
+  * Generic function to open text files and process them
+  */
+  function openFile(properties){
+    'use strict';
+
+    // Creates the request
+    var 
+      request = new XMLHttpRequest(),
+
+      file = properties.file,
+      callbackFunction = properties.callback,
+      format = properties.format || file.split('.').slice(-1)[0],
+      id = properties.id || null
+    ;
+    
+    // Try opens the file using GET method
+    try{
+      request.open('GET', file);
+    } catch(err){
+      console.error(err);
+      console.log("Attempt failed!");
+      return -1;
+    }
+    
+    // Sends the request
+    request.send();
+    
+    // When file is opened
+    request.onreadystatechange = function () {
+      // If we already have a full response
+      if (request.readyState === 4) {
+        try{
+            callbackFunction({content: request.responseText, id: id, file: file, fileFormat: format});
+        } catch (err){
+          console.error(err);
+          console.log("No function was passed as argument.");
+          return -1;
+        }
+      }
+    };
+      
+    return request;
+  }
+
+  /*
+  * Searches for local links and turn them in to absolute paths
+  */
+  function relativize(content){
+    var 
+      content ,
+        
+      link,
+      curLocation = window.location,
+      rtr,
+      
+      // Defines a link as something that:
+      // Starts with ./ OR ../ OR /
+      // Don't have //
+      // Ends with a word or /
+      regex = /^(\u002E{1,2}\u002F|\u002F{1})[^\u002F].{1,}(\u002F|\w{1,})$/gm,
+      hName = curLocation.hostname,
+      pName = curLocation.pathname.replace('/','').split('/')[0];
+    
+    rtr = content;
+    
+    // Splits the content by " " or "[" or "]" or "(" or ")" or "{" or "}"
+    content = content.split(/( |\u0028|\u0029|\u005B|\u005D|\u007B|\u007D)/);
+    
+    // Search for links
+    for(let str in content){
+      // Test if it is a link
+      if(regex.test(content[[str]])){
+        link = content[str];
+          
+        var
+          newLink = Bonisa.location + '/' + link.replace(/\u002E+\u002F/g, "");
+
+        rtr = rtr.replace(link, newLink);
+      }
+    }
+    
+    return rtr;
+  }
   
   return Bonisa;
 }());
